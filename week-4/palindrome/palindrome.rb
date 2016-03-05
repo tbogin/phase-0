46c7868af2361d9abee878a630bec7c6153b0e75
tbogin <tbogin@gmail.com>
2016-03-05 15:32:24 +0000

    Practice makes perfect

---
+++ week-4/palindrome/palindrome.rb
@@ -0,0 +1,9 @@
+
+song = 10
+while song > 1
+puts "#{song.to_s} bottles of beer on the wall"
+puts "#{song.to_s} bottles of beer"
+song = song -1
+puts "Take one down, pass it around"
+puts "#{song.to_s} bottles of beer on the wall"
+end
