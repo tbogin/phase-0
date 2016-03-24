2f4e1429fe4fdb8ffad23a55538ae4a717e9d9ea
tbogin <tbogin@gmail.com>
2016-03-24 15:12:00 +0000

    Solve challenge

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,22 @@
+
+/*
+Pseudocode - create 3 functions to apply to an array of numbers
+
+Create a function called 'sum' 
+
+sum will have an array of numbers as its argument
+
+sum will add the numbers within that array
+
+--------------------------------------------------
+
+Create a new function called 'mean'
+
+mean will average all the numbers within the array of numbers
+
+--------------------------------------------------------
+
+Create a function called 'median'
+
+median will find the middle value from the array of numbers. If there is no single middle number, the function should average the 2 middle numbers
+*/
